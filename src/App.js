418a174aa0d2229c2dd00a5fa18932def35fdf12
Daniel Čonka <pykejavzlykej@gmail.com>
2023-10-22 17:49:16 +0200

    Uprava1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SharedLayaut from "./pages/strukturaStranky/SharedLayaut"
+import Home from "./pages/Home"
 import ChciNabidku from "./pages/ChciNabidku"
 import VypsatVyber from "./pages/VypsatVyber"
 import Error from "./pages/Error"
@@ -8,7 +9,8 @@ const App = () => {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<SharedLayaut />}>
-        <Route index path="/chci-nabidku" element={<ChciNabidku />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="/chci-nabidku" element={<ChciNabidku />}></Route>
         <Route path="/vypsat-nabidky" element={<VypsatVyber />}></Route>
         <Route path="*" element={<Error />}/>
       </Route>
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import "./css/Home.css"
+
+const Home = () => {
+  return <div className="home">
+    <strong>Vyberte jednu z možností v menu</strong>
+
+  </div>
+}
+
+export default Home
